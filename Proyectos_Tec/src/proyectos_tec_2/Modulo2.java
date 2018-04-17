4682117b0d310af8a265e779c6b3d2363b20ffc6
Mrkemey <jmanuelwlf@hotmail.com>
2018-04-17 16:08:01 -0600

    Merge origin/origin/master into origin/master

---
+++ Proyectos_Tec/src/proyectos_tec_2/Modulo2.java
@@ -0,0 +1,46 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package proyectos_tec_2;
+
+import static javax.swing.JOptionPane.*;
+
+/**
+ *
+ * @author jmanu
+ */
+public class Modulo2 {
+    public static void main(String[] args) {
+//        int n=Integer.parseInt(showInputDialog("Ingrese un numero"));
+//        String b=Modulo1.EjemploCiclo(n);
+//        showMessageDialog(null,b);
+//-------------------------------------------------        
+//        String b=showInputDialog("oct");
+//        int n=Modulo1.octDec(b);
+//        showMessageDialog(null, n+"="+b);
+//-------------------------------------------------        
+//     int n=88;
+//     String b=Modulo1.decOct(n);
+//     showMessageDialog(null, n+"="+b);
+//-------------------------------------------------        
+//        String h=showInputDialog("hex");
+//        int n=Modulo1.hexDec(h);
+//        showMessageDialog(null, h+"="+n);
+//-------------------------------------------------        
+//     int n=78;
+//     String b=Modulo1.decHEX(n);
+//     showMessageDialog(null, n+"="+b);
+//-------------------------------------------------        
+//        String b=showInputDialog("bin");
+//        int n=Modulo1.binDec(b);
+//        showMessageDialog(null, n+"="+b);
+//-------------------------------------------------        
+     int n=12;
+     String b=Modulo1.decBin(n);
+     showMessageDialog(null, n+"="+b);
+//-------------------------------------------------        
+//        showMessageDialog(null, Modulo1.dobleNum(40));
+    }
+}
